2f68d3a58dc9ae2d846aa54165aadc37457560a4
Chris Manus <christtroopermanus106@student.unsrat.ac.id>
2023-04-15 18:12:20 +0800

    first commit

---
+++ pages/php/index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="../../assets/css/style.css">
+  <title>Document</title>
+</head>
+
+<body>
+  <?php
+  // Title
+  echo "<h1 class='title-h1'>Tugas Arkatama</h1>
+
+  <div class='biodata'>
+    <h2>Data Diri</h2>
+    <p>Nama : Christtrooper Manus</p>
+    <p>Tempat Tanggal Lahir : Manado 08 Juni 2003</p>
+    <p>Jenis Kelamin : Laki - Laki</p>
+    <p>Alamat : Sulawesi Utara, Kabupaten Minahasa, Desa Tonsealama</p>
+    <div class='biodata-foto'>
+        <img style='width: 354px; height: 472px; border: 2px black solid;' src='' alt=''/>
+        <h3>Foto 3x4</h3>
+    </div>
+    <p>Riwayat Pendidikan : </p>
+    <ul>
+      <li>SD 3 Tondano</li>
+      <li>SMP 1 Tondano</li>
+      <li>SMA 3 Tondano</li>
+    </ul>
+    <p>Hobby : Bermain Video Game</p>
+  </div>
+  ";
+  ?>
+</body>
+
+</html>
